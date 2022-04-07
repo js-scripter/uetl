3450270eda13f1ec45ce592f0210b9ac26bd39f3
sachin Gaikwad <sachin.gaikwad@cympl.local>
2022-04-07 21:03:29 +0530

    handled scenarios like empty directory in  cron job updated views with bootstrap CSS

--- app.js
+++ app.js
@@ -3,13 +3,13 @@ const multer  = require('multer')
 const upload = multer({ dest: 'uploads/' })
 const appRoot = require("app-root-path");
 const path = require('path');
-const port = process.env.PORT || 3000
 const scheduler =require('./scheduler')
-
+const port = process.env.PORT || 3000
 const app = express()
 
-// cron job to run every 1 minute to check for new file and upload it to the DB
-scheduler.scheduleUpload();
+// cron job to run asynchronously, non blobking way, every 1 minute 
+// to check for new file and upload it to the DB
+scheduler.scheduleETL();
 
 app.get("/", (request, response) => {
         let pathUri = `${appRoot}/client/home.html`;
@@ -18,11 +18,12 @@ app.get("/", (request, response) => {
 )
 
 app.post('/uploadBooks', upload.single('books'), (req, res, next)=> {
-  // req.file is the `avatar` file
+  // req.file is the `books` file
   // req.body will hold the text fields, if there were any
   console.log(req.file)
+  // after upload is complete send user a upload status view 
   let pathUri = `${appRoot}/client/uploadStatus.html`;
-  response.sendFile(path.resolve(pathUri));
+  res.sendFile(path.resolve(pathUri));
 })
 
 
--- scheduler/index.js
+++ scheduler/index.js
@@ -1,10 +1,33 @@
 // import dbconn  from "../connections/dbconn";
 const schedule = require('node-schedule');
+const randomFile = require('select-random-file')
+const fs = require('fs');
+const appRoot = require("app-root-path");
+const path = require('path');
+const dir = 'uploads/'
+let pathUri = `${appRoot}/uploads/`;
 
-module.exports.scheduleUpload = async function (msg) { 
+module.exports.scheduleETL = async function (msg) { 
     //we can take schedule value from config which is best practice but here we are hardcoding
     const job = schedule.scheduleJob('*/1 * * * *', async function(){
-        console.log('The answer to life, the universe, and everything! every minute');
+        randomFile(dir, (err, file) => {
+          // if directory is empty return
+          if(file==undefined){
+            console.log('no file found to process and delete')
+            return
+          }
+
+          // as file is not undefined/null start processing it
+
+          // after processing the file delete it ascychronously, non blocking way
+          fs.unlink(pathUri+file, (err) => {
+            if (err) {
+                console.log(err);
+                return
+            }
+            console.log("File is deleted.");
+            });
+          })
     })
 };
 
